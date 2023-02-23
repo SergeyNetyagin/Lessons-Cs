0473ae36e13ad92f077a0b8e0a96a6d559f590de
SergeyNetyagin <34445205+SergeyNetyagin@users.noreply.github.com>
2023-02-23 16:48:07 +0300

    First solution has been created

---
+++ Lessons-cs/WinForms/MainForm.cs
@@ -0,0 +1,11 @@
+namespace WinForms {
+	public partial class MainForm : Form {
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void MainFormLoad( object sender, EventArgs e ) {
+
+		}
+	}
+}
